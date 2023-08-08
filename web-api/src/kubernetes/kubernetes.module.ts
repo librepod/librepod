8af5ecbadfed632ed0a7e095a0bf2ffc4d6e9d33
Igor Lukashenya <igorlukashenya@coherentsolutions.com>
2023-08-08 19:05:11 +0300

    LP-1: Update imports.

--- web-api/src/kubernetes/kubernetes.module.ts
+++ web-api/src/kubernetes/kubernetes.module.ts
@@ -1,5 +1,5 @@
 import { Module } from '@nestjs/common';
-import { KubernetesService } from './kubernetes.service';
+import { KubernetesService } from './services/kubernetes.service';
 
 @Module({
   providers: [KubernetesService],
